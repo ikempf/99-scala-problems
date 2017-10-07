d0096a7b2ee5f04b0908c43bdd19dfae19ea51e3
Ilja <ilja.kempf@gmail.com>
2017-10-07 22:05:30 +0200

    Problem 001

---
+++ src/test/scala/com/ikempf/Problem001Test.scala
@@ -0,0 +1,12 @@
+package com.ikempf
+
+import com.ikempf.Problem001.last
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem001Test extends FlatSpec with Matchers {
+
+  "Last" should "find the last element of a list" in {
+    last(List(1, 2, 3)) should equal(3)
+  }
+
+}
