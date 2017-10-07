e4aae87180de16a6ed1b7f9b35c452fbfcb0c891
Ilja <ilja.kempf@gmail.com>
2017-10-07 22:05:57 +0200

    Problem 004

---
+++ src/test/scala/com/ikempf/Problem004Test.scala
@@ -0,0 +1,12 @@
+package com.ikempf
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem004Test extends FlatSpec with Matchers {
+
+  "Length" should "find the number of elements of the list" in {
+    Problem004.length(List(1, 2, 3)) should equal(3)
+    Problem004.length(List()) should equal(0)
+  }
+
+}
