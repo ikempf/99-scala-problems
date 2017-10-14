8f6ffa7c754b24b9f4c10e0b6aa42e85ae2b382a
Ilja <ilja.kempf@gmail.com>
2017-10-14 14:54:17 +0200

    Problem 016

---
+++ src/test/scala/com/ikempf/Problem016Test.scala
@@ -0,0 +1,15 @@
+package com.ikempf
+
+import com.ikempf.Problem016.{drop, dropEasy, dropEasy2, dropEasy3}
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem016Test extends FlatSpec with Matchers {
+
+  "Drop" should "drop nth element of the given list" in {
+    drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    dropEasy(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    dropEasy2(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    dropEasy3(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+  }
+
+}
