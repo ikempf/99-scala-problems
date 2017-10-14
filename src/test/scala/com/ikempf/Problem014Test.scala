c155d0a5ab62afd9e1fcb39a14ab4709ec04dc62
Ilja <ilja.kempf@gmail.com>
2017-10-14 14:53:55 +0200

    Problem 014

---
+++ src/test/scala/com/ikempf/Problem014Test.scala
@@ -0,0 +1,16 @@
+package com.ikempf
+
+import com.ikempf.Problem014.{duplicate, duplicateEasy}
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem014Test extends FlatSpec with Matchers {
+
+  "Duplicate" should "duplicate elements of a list" in {
+    duplicate(List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+
+  "DuplicateFold" should "duplicate elements of a list" in {
+    duplicateEasy(List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+
+}
