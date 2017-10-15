6ba5305437c6282d27cfd20e029b684d1e1fa43a
Ilja <ilja.kempf@gmail.com>
2017-10-15 12:19:37 +0200

    Problem 018 fix order of match cases

--- src/main/scala/com/ikempf/Problem018.scala
+++ src/main/scala/com/ikempf/Problem018.scala
@@ -4,8 +4,8 @@ object Problem018 {
 
   def slice[A](start: Int, end: Int, l: List[A]): List[A] =
     (start, end, l) match {
-      case (0, m, h :: t)          ⇒ h +: slice(0, m - 1, t)
       case (_, 0, _) | (_, _, Nil) ⇒ Nil
+      case (0, m, h :: t)          ⇒ h +: slice(0, m - 1, t)
       case (n, m, _ :: t)          ⇒ slice(n - 1, m - 1, t)
     }
 
