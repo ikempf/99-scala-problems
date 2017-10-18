c75fa15e73eae645c96fa813b64b14f774030231
Ilja <ilja.kempf@gmail.com>
2017-10-18 22:17:52 +0200

    Problem 026

---
+++ src/main/scala/com/ikempf/Problem026.scala
@@ -0,0 +1,19 @@
+package com.ikempf
+
+object Problem026 {
+
+  // super hard :/
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else
+      flatMapSublists(ls) { sl ⇒
+        combinations(n - 1, sl.tail) map { sl.head :: _ }
+      }
+
+  def flatMapSublists[A, B](ls: List[A])(f: (List[A]) ⇒ List[B]): List[B] =
+    ls match {
+      case Nil                   ⇒ Nil
+      case sublist @ (_ :: tail) ⇒ f(sublist) ::: flatMapSublists(tail)(f)
+    }
+
+}
